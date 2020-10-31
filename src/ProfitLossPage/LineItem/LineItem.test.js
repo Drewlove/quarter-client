704e7f701ea59987c9f11e3683747da16266b9f1
Drew Love <davidandrewlove84@gmail.com>
2020-10-31 16:35:33 -0400

    LineItem tests, shallow render and display two p elements

--- src/ProfitLossPage/LineItem/LineItem.test.js
+++ src/ProfitLossPage/LineItem/LineItem.test.js
@@ -1,27 +1,26 @@
 import React from 'react'; 
-import ReactDOM from 'react-dom';
-import LineItem from './LineItem'
-import {CapitalizeAllWords, Hyphenate} from '../../Utilities/UtilityFunctions'
+import { shallow, mount } from "enzyme";
+import LineItem from "./LineItem";
 
 const category = 'direct labor'
 const name = 'service shift leader'
 const amount = 1000
 
 describe('LineItem', ()=> {
-    it('renders without crashing', () => {
-        const div = document.createElement('div')
-        ReactDOM.render(
-            <LineItem name={name} category={category} amount={amount}/>, div
-        )
-        ReactDOM.unmountComponentAtNode(div)
+    it('renders', () => {
+        const wrapper = shallow(<LineItem category='category' name='name' amount={200}/>)
     })
-    it('CapitalizeAllWords the category name', () => {
-        expect(CapitalizeAllWords(category)).toBe('Direct Labor');
+    it('displays two paragraph elements', ()=> {
+        const wrapper = mount(<LineItem category='category' name='name' amount={200}/>); 
+        expect(wrapper.find('.pl-fieldset__item-text')).toHaveLength(2);
     })
-    it('formats category total with commas', () => {
-        expect(amount.toLocaleString()).toBe('1,000');
-    })
-    it('formats the href string correctly', () => {
-        expect(`/form/${Hyphenate(category)}/${Hyphenate(name)}`.toLocaleString()).toBe('/form/direct-labor/service-shift-leader');
-    })
+    // it('CapitalizeAllWords the category name', () => {
+    //     expect(CapitalizeAllWords(category)).toBe('Direct Labor');
+    // })
+    // it('formats category total with commas', () => {
+    //     expect(amount.toLocaleString()).toBe('1,000');
+    // })
+    // it('formats the href string correctly', () => {
+    //     expect(`/form/${Hyphenate(category)}/${Hyphenate(name)}`.toLocaleString()).toBe('/form/direct-labor/service-shift-leader');
+    // })
 })
