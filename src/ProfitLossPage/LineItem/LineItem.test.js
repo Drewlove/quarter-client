5f39daf0f83f21bd925dd3ed102fe7b633f0862d
Drew Love <davidandrewlove84@gmail.com>
2020-10-31 16:45:08 -0400

    LineItem test, formats href value and capitalizes line item name

--- src/ProfitLossPage/LineItem/LineItem.test.js
+++ src/ProfitLossPage/LineItem/LineItem.test.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { shallow, mount } from "enzyme";
 import LineItem from "./LineItem";
 
-const category = 'direct labor'
-const name = 'service shift leader'
-const amount = 1000
-
 describe('LineItem', ()=> {
     it('renders', () => {
         const wrapper = shallow(<LineItem category='category' name='name' amount={200}/>)
@@ -14,13 +10,12 @@ describe('LineItem', ()=> {
         const wrapper = mount(<LineItem category='category' name='name' amount={200}/>); 
         expect(wrapper.find('.pl-fieldset__item-text')).toHaveLength(2);
     })
-    // it('CapitalizeAllWords the category name', () => {
-    //     expect(CapitalizeAllWords(category)).toBe('Direct Labor');
-    // })
-    // it('formats category total with commas', () => {
-    //     expect(amount.toLocaleString()).toBe('1,000');
-    // })
-    // it('formats the href string correctly', () => {
-    //     expect(`/form/${Hyphenate(category)}/${Hyphenate(name)}`.toLocaleString()).toBe('/form/direct-labor/service-shift-leader');
-    // })
+    it('capitalizes line item name', () => {
+        const wrapper = mount(<LineItem category='category' name='name' amount={200}/>); 
+        expect(wrapper.find('.pl-fieldset__item-text').at(0).text()).toEqual('Name')
+    })
+    it('formats href correctly', () => {
+        const wrapper = mount(<LineItem category='category' name='name' amount={200}/>); 
+        expect(wrapper.find('a').props().href).toEqual('/form/category/name/')
+    })
 })
