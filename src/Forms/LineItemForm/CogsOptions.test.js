02386ee522bcd042a5b7724ac0ac3061ef324557
Drew Love <davidandrewlove84@gmail.com>
2020-11-02 19:15:45 -0500

    it renders amountType radio when COGS is selected, it renders drop down for sales type when percent is selected

--- src/Forms/LineItemForm/CogsOptions.test.js
+++ src/Forms/LineItemForm/CogsOptions.test.js
@@ -1,9 +1,14 @@
-import React from "react";
-import { shallow, mount} from "enzyme";
+import React from 'react'; 
+import { shallow, mount } from "enzyme";
 import CogsOptions from "./CogsOptions";
 
 describe('CogsOptions', () => {
+    let wrapper; 
+    beforeEach( () => {
+        wrapper = shallow(<CogsOptions />)
+    })
+
     it('renders', () => {
-        shallow(<CogsOptions />)
+        wrapper; 
     })
 })
--- src/Forms/LineItemForm/LineItemForm.test.js
+++ src/Forms/LineItemForm/LineItemForm.test.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Enzyme from "enzyme";
 import { shallow, mount } from "enzyme";
 import LineItemForm from "./LineItemForm";
 
@@ -13,7 +12,7 @@ jest.mock("react-router-dom", () => ({
 describe("LineItemForm", () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = Enzyme.mount(<LineItemForm />);
+    wrapper = mount(<LineItemForm />);
   });
 
   it("renders", () => {
@@ -23,16 +22,24 @@ describe("LineItemForm", () => {
   it("renders two inputs", () => {
     expect(wrapper.find("input")).toHaveLength(2);
   });
+
   it("correctly updates the value on select", () => {
-    const categoryDropDown = wrapper.find("#category");
     const event = { target: { name: "category", value: "sales" } };
-    categoryDropDown.simulate("change", event);
+    wrapper.find("#category").simulate("change", event);
     expect(wrapper.find("#category").props().value).toEqual("sales");
   });
+  
   it("renders radio button container if category is COGS", () => {
-    const categoryDropDown = wrapper.find("#category");
     const event = { target: { name: "category", value: "cogs" } };
-    categoryDropDown.simulate("change", event);  
+    wrapper.find("#category").simulate("change", event);  
     expect(wrapper.find('.input-container__radio')).toHaveLength(1) 
   });
+
+  it("it renders 'Percent Of' drop down if amountType 'percent' is selected", () => {
+    const eventCogs = { target: { name: "category", value: "cogs" } };
+    const eventPercentage = { target: { name: "amountType", value: "percentage" } };
+    wrapper.find("#category").simulate("change", eventCogs); 
+    wrapper.find("#percentage").simulate("change", eventPercentage); 
+    expect(wrapper.find('#percentage').prop('checked')).toBe(true)
+  }); 
 });
