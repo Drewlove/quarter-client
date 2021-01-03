937a7c5c90a34f81981c4e30fb300f181b98bab8
Drew Love <davidandrewlove84@gmail.com>
2021-01-03 12:25:37 -0500

    API_KEY and env variable work

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 export default {
   API_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:9000/api"
-      : "https://limitless-stream-84911.herokuapp.com/api",
+  API_ENDPOINT: process.env.SERVER,
+  //   API_ENDPOINT:
+  //     process.env.NODE_ENV === "development"
+  //       ? "http://localhost:9000/api"
+  //       : "https://limitless-stream-84911.herokuapp.com/api",
   HEADERS: {
     Authorization: process.env.REACT_APP_API_KEY,
     "Content-Type": "application/json",
