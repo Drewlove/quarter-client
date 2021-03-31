326267bba5a06cac8afb2905a4aa1a5a58f04575
Drew Love <davidandrewlove84@gmail.com>
2021-03-31 09:43:17 -0400

    remove API token from headers

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   API_ENDPOINT: process.env.REACT_APP_SERVER,
   HEADERS: {
-    Authorization: process.env.REACT_APP_API_KEY,
+    // Authorization: process.env.REACT_APP_API_KEY,
     "Content-Type": "application/json",
   },
 };
