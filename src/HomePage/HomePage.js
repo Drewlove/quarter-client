4a3fdec0ccd131799c04de197a572db8fbdadeb7
Drew Love <davidandrewlove84@gmail.com>
2021-04-12 17:00:45 -0400

    Add favicon

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -42,6 +42,16 @@ function HomePage() {
             </p>
           </div>
         </section>
+        <div>
+          Icons made by{" "}
+          <a href="" title="bqlqn">
+            bqlqn
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
       </main>
     </>
   );
