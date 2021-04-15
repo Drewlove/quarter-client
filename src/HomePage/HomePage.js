60c90c07955545f822af90f08165a74338d00af2
Drew Love <davidandrewlove84@gmail.com>
2021-04-15 16:25:23 -0400

    Add icon pictures to home page

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,4 +1,8 @@
 import React from "react";
+import PnlIcon from "../Assets/pnl.png";
+import ScheduleIcon from "../Assets/schedule.png";
+import COGSIcon from "../Assets/cogs.png";
+import AccountingIcon from "../Assets/accounting.png";
 import SignUpButton from "../Authentication/SignUpButton/SignUpButton";
 
 function HomePage() {
@@ -6,52 +10,65 @@ function HomePage() {
     <>
       <main className="main main_home-page">
         <section className="home-page">
-          <div>
-            <h2 className="home-page__h2">Quarter</h2>
-            <p className="home-page__text">
-              Model your business for easy quarterly P&L projections.
-            </p>
-            <div className="home-page__hero-image"></div>
+          <h2 className="home-page__h2">Quarter</h2>
+          <p className="home-page__tagline">
+            Model your food business with easy quarterly P&L projections.
+          </p>
+          <div className="home-page__container">
+            <img
+              className="home-page__img"
+              alt="profit and loss icon"
+              src={PnlIcon}
+            />
+            <div className="home-page__divider"></div>
+
             <SignUpButton optionalClass="home-page__user-link_authentication" />
           </div>
           <div className="home-page__container">
             <div className="home-page__divider"></div>
             <h3 className="home-page__h3">Scheduling</h3>
-            <div className="home-page__promo-image"></div>
+            <img
+              className="home-page__img"
+              alt="schedule icon"
+              src={ScheduleIcon}
+            />
             <p className="home-page__text">
-              Evaluate how schedule changes affect your labor costs.
+              Evaluate how schedule changes affect labor costs.
             </p>
           </div>
           <div className="home-page__container">
             <div className="home-page__divider"></div>
             <h3 className="home-page__h3">COGS</h3>
-            <div className="home-page__promo-image"></div>
+            <img className="home-page__img" alt="COGS icon" src={COGSIcon} />
             <p className="home-page__text">
-              Set your COGS % to determine profitability as sales volume
-              increases.
+              Set a COGS % for sales categories to determine profitability.
             </p>
           </div>
           <div className="home-page__container">
             <div className="home-page__divider"></div>
             <h3 className="home-page__h3">Quarterly</h3>
-            <div className="home-page__promo-image"></div>
+            <img
+              className="home-page__img"
+              alt="accounting icon"
+              src={AccountingIcon}
+            />
             <p className="home-page__text">
               Weekly payroll, monthly rent, and more are all accounted for in a
-              4-4-5 accounting cycle.
+              13 week quarterly cycle.
             </p>
           </div>
         </section>
-        <footer className="footer">
-          Icons made by bqlqn from{" "}
-          <a
-            className="footer__link"
-            href="https://www.flaticon.com/"
-            title="Flaticon"
-          >
-            Flaticon
-          </a>
-        </footer>
       </main>
+      <footer className="footer">
+        Icons made by bqlqn, freepick, mangsaabguru, and srip from{" "}
+        <a
+          className="footer__link"
+          href="https://www.flaticon.com/"
+          title="Flaticon"
+        >
+          Flaticon
+        </a>
+      </footer>
     </>
   );
 }
